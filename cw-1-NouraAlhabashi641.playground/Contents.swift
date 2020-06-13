fe0c046dd832d13bd547c62c48d62105daf9c2cc
n0bxii <kimnoura8@gmail.com>
2020-06-13 22:00:36 +0300

    CW-1 DONE

---
+++ cw-1-NouraAlhabashi641.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "Noura"
+
+var hobby1 = "reading"
+var hobby2 = "walking"
+var age = 15
+var hight = 148
+print("MY NAME IS \(name) My age is \(age) My hobbies are \(hobby1) and \(hobby2) my hight is \(hight) ")
+
+//
+var friendname = " fouz "
+var friendage = 15
+var friendhobby1 = " eating "
+var friendhobby2 = " walking "
+var friendhight = 140
+//
+hobby1 == friendhobby1
+hobby1 != friendhobby1
+hobby2 == friendhobby2
+hobby2 != friendhobby2
+
+age > friendage
+age < friendage
+age >= friendage
+age <= friendage
+age == friendage
+age != friendage
+
+hight == friendhight
+hight < friendhight
+hight > friendhight
+hight <= friendhight
+hight >= friendhight
+hight != friendhight
+
+
+
+
+
